8c62503d855514e457b61406a35b0201cbc2c1ef
amirmsj1374 <amir.msj74@gmail.com>
2021-07-05 17:17:19 +0430

    Add: vazir font Add: setting module in vuex

---
+++ store/setting/state.js
@@ -0,0 +1,8 @@
+export default function () {
+  return {
+    barColor: '#00CAE3',
+    barImage: 'https://demos.creative-tim.com/material-dashboard/assets/img/sidebar-1.jpg',
+    mode:false,
+    sidebarImage: true,
+  }
+}
